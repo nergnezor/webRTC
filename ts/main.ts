544997d64b82094be54de08569f5f86030bb322c
nergnezor <nergnezor@users.noreply.github.com>
2020-03-27 17:16:42 +0000

    deploy: de69c04e66205027398ea3e5be8fef0eddb732af

--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,30 @@
-const videoElem = document.getElementById("video") as HTMLVideoElement;
 const logElem = document.getElementById("log");
-const startElem = document.getElementById("start");
-const stopElem = document.getElementById("stop");
+const startElem = document.getElementById("start") as HTMLButtonElement;
+const stopElem = document.getElementById("stop") as HTMLButtonElement;
+
+const videoSources: HTMLVideoElement[] = [];
+const servers = null;
+const pc1: RTCPeerConnection = new RTCPeerConnection(servers);
+const remoteStream = new MediaStream();
+const remoteVideo = document.getElementById("remoteVideo") as HTMLVideoElement;
+// pc1.addEventListener("track", async event => {
+//   console.info("pc1.addEventListener");
+//   remoteVideo.srcObject = remoteStream;
+//   remoteStream.addTrack(event.track);
+// });
+pc1.ontrack = e => {
+  console.info("pc1.addEventListener");
+
+  return false;
+};
+const pc2 = new RTCPeerConnection(servers);
+pc2.ontrack = gotRemoteStream;
+function gotRemoteStream(e) {
+  console.info("gotRemoteStream");
+
+  remoteVideo.srcObject = e.stream;
+}
+
 console.log = msg => (logElem.innerHTML += `${msg}<br>`);
 console.error = msg =>
   (logElem.innerHTML += `<span class="error">${msg}</span><br>`);
@@ -9,18 +32,27 @@ console.warn = msg =>
   (logElem.innerHTML += `<span class="warn">${msg}<span><br>`);
 console.info = msg =>
   (logElem.innerHTML += `<span class="info">${msg}</span><br>`);
-// Options for getDisplayMedia()
 
 var displayMediaOptions = {
   video: true,
   audio: false
 };
 
-// Set event listeners for the start and stop buttons
+// const desc: RTCSessionDescription = {
+//   sdp = "",
+//   type = null,
+//   toJSON
+// };
+
+function UpdateButtons() {
+  stopElem.hidden = videoSources.length == 0;
+}
+
 startElem.addEventListener(
   "click",
   function(evt) {
     startCapture();
+    UpdateButtons();
   },
   false
 );
@@ -29,12 +61,13 @@ stopElem.addEventListener(
   "click",
   function(evt) {
     stopCapture();
+    UpdateButtons();
   },
   false
 );
 
 function gotLocalMediaStream(mediaStream: MediaStream) {
-  videoElem.srcObject = mediaStream;
+  videoSources[videoSources.length - 1].srcObject = mediaStream;
   dumpOptionsInfo();
 }
 async function error(error: Error) {
@@ -43,13 +76,14 @@ async function error(error: Error) {
 
 async function startCapture() {
   let video = document.createElement("video");
-  let video2 = video as HTMLVideoElement;
-  let div = document.getElementById("videos");
-  div.appendChild(video2);
+  video.autoplay = true;
+  video.width = 200;
+  // video.
+  videoSources.push(video);
   logElem.innerHTML = "";
   try {
     // @ts-ignore
-    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(
+    video.srcObject = await navigator.mediaDevices.getDisplayMedia(
       displayMediaOptions
     );
     dumpOptionsInfo();
@@ -59,20 +93,38 @@ async function startCapture() {
       .getUserMedia(displayMediaOptions)
       .then(gotLocalMediaStream)
       .catch(error);
+    return;
+  } finally {
+    let div = document.getElementById("videos");
+    div.appendChild(video);
+    let tracks = (videoSources[videoSources.length - 1]
+      .srcObject as MediaStream).getTracks();
+    tracks.forEach(track => {
+      console.info("track: " + track);
+      pc1.addTrack(track, video.srcObject as MediaStream);
+    });
   }
 }
 
 function stopCapture() {
-  let tracks = (videoElem.srcObject as MediaStream).getTracks();
+  let tracks = (videoSources[videoSources.length - 1]
+    .srcObject as MediaStream).getTracks();
 
   tracks.forEach(track => track.stop());
-  videoElem.srcObject = null;
+  document
+    .getElementById("videos")
+    .removeChild(videoSources[videoSources.length - 1]);
+  videoSources[videoSources.length - 1].srcObject = null;
+  videoSources.pop();
 }
 function dumpOptionsInfo() {
-  const videoTrack = (videoElem.srcObject as MediaStream).getVideoTracks()[0];
+  const videoTrack = (videoSources[videoSources.length - 1]
+    .srcObject as MediaStream).getVideoTracks()[0];
 
   console.info("Track settings:");
   console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
   console.info("Track constraints:");
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
+
+UpdateButtons();
