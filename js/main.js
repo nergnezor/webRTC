e57500b51b7bf04453968e87e6ae914b8751a227
nergnezor <nergnezor@users.noreply.github.com>
2020-03-27 11:32:19 +0000

    deploy: e083e767a1d2737bb4b5e7500db13f8a1c5b11a2

--- js/main.js
+++ js/main.js
@@ -3,71 +3,53 @@ const logElem = document.getElementById("log");
 const startElem = document.getElementById("start");
 const stopElem = document.getElementById("stop");
 console.log = msg => (logElem.innerHTML += `${msg}<br>`);
-console.error = msg =>
-  (logElem.innerHTML += `<span class="error">${msg}</span><br>`);
-console.warn = msg =>
-  (logElem.innerHTML += `<span class="warn">${msg}<span><br>`);
-console.info = msg =>
-  (logElem.innerHTML += `<span class="info">${msg}</span><br>`);
+console.error = msg => (logElem.innerHTML += `<span class="error">${msg}</span><br>`);
+console.warn = msg => (logElem.innerHTML += `<span class="warn">${msg}<span><br>`);
+console.info = msg => (logElem.innerHTML += `<span class="info">${msg}</span><br>`);
 // Options for getDisplayMedia()
-
 var displayMediaOptions = {
-  video: true,
-  audio: false
+    video: true,
+    audio: false
 };
-
 // Set event listeners for the start and stop buttons
-startElem.addEventListener(
-  "click",
-  function(evt) {
+startElem.addEventListener("click", function (evt) {
     startCapture();
-  },
-  false
-);
-
-stopElem.addEventListener(
-  "click",
-  function(evt) {
+}, false);
+stopElem.addEventListener("click", function (evt) {
     stopCapture();
-  },
-  false
-);
-
+}, false);
 function gotLocalMediaStream(mediaStream) {
-  videoElem.srcObject = mediaStream;
-  dumpOptionsInfo();
+    videoElem.srcObject = mediaStream;
+    dumpOptionsInfo();
 }
 async function handleLocalMediaStreamError(error) {
-  console.error("navigator.getUserMedia error: ", error.message);
+    console.error(error);
 }
-
 async function startCapture() {
-  logElem.innerHTML = "";
-  try {
-    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(
-      displayMediaOptions
-    );
-    dumpOptionsInfo();
-  } catch (err) {
-    console.error("Error: " + err);
-    navigator.mediaDevices
-      .getUserMedia(displayMediaOptions)
-      .then(gotLocalMediaStream)
-      .catch(console.error("Error: " + err));
-  }
+    logElem.innerHTML = "";
+    try {
+        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        dumpOptionsInfo();
+    }
+    catch (err) {
+        handleLocalMediaStreamError(err);
+        navigator.mediaDevices
+            .getUserMedia(displayMediaOptions)
+            .then(gotLocalMediaStream)
+            .catch(handleLocalMediaStreamError);
+        // .catch(console.error("Error: " + err));
+    }
 }
-
-function stopCapture(evt) {
-  let tracks = videoElem.srcObject.getTracks();
-
-  tracks.forEach(track => track.stop());
-  videoElem.srcObject = null;
+function stopCapture() {
+    let tracks = videoElem.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
 }
 function dumpOptionsInfo() {
-  const videoTrack = videoElem.srcObject.getVideoTracks()[0];
-
-  console.info("Track settings:");
-  console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
-  console.info("Track constraints:");
-  console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+    const videoTrack = videoElem.srcObject.getVideoTracks()[0];
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
+//# sourceMappingURL=main.js.map
