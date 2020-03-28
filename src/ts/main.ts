7ff1f4fb80b1e5fdd7bd01e519c88e58cc46c2e2
nergnezor <e@roseng.ren>
2020-03-28 09:36:16 +0100

    Multi streaming through PeerConnection

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,29 +1,8 @@
 const logElem = document.getElementById("log");
 const startElem = document.getElementById("start") as HTMLButtonElement;
 const stopElem = document.getElementById("stop") as HTMLButtonElement;
-
+const sourcesElem = document.getElementById("videos");
 const videoSources: HTMLVideoElement[] = [];
-const servers = null;
-const pc1: RTCPeerConnection = new RTCPeerConnection(servers);
-const remoteStream = new MediaStream();
-const remoteVideo = document.getElementById("remoteVideo") as HTMLVideoElement;
-// pc1.addEventListener("track", async event => {
-//   console.info("pc1.addEventListener");
-//   remoteVideo.srcObject = remoteStream;
-//   remoteStream.addTrack(event.track);
-// });
-pc1.ontrack = e => {
-  console.info("pc1.addEventListener");
-
-  return false;
-};
-const pc2 = new RTCPeerConnection(servers);
-pc2.ontrack = gotRemoteStream;
-function gotRemoteStream(e) {
-  console.info("gotRemoteStream");
-
-  remoteVideo.srcObject = e.stream;
-}
 
 console.log = msg => (logElem.innerHTML += `${msg}<br>`);
 console.error = msg =>
@@ -37,12 +16,6 @@ var displayMediaOptions = {
   video: true,
   audio: false
 };
-
-// const desc: RTCSessionDescription = {
-//   sdp = "",
-//   type = null,
-//   toJSON
-// };
 
 function UpdateButtons() {
   stopElem.hidden = videoSources.length == 0;
@@ -75,8 +48,9 @@ async function error(error: Error) {
 }
 
 async function startCapture() {
-  let video = document.createElement("video");
+  let video = document.createElement("video") as HTMLVideoElement;
   video.autoplay = true;
+  video.className = "server";
   video.width = 200;
   // video.
   videoSources.push(video);
@@ -95,14 +69,8 @@ async function startCapture() {
       .catch(error);
     return;
   } finally {
-    let div = document.getElementById("videos");
-    div.appendChild(video);
-    let tracks = (videoSources[videoSources.length - 1]
-      .srcObject as MediaStream).getTracks();
-    tracks.forEach(track => {
-      console.info("track: " + track);
-      pc1.addTrack(track, video.srcObject as MediaStream);
-    });
+    sourcesElem.appendChild(video);
+    call(video.srcObject as MediaStream);
   }
 }
 
@@ -111,11 +79,13 @@ function stopCapture() {
     .srcObject as MediaStream).getTracks();
 
   tracks.forEach(track => track.stop());
-  document
-    .getElementById("videos")
-    .removeChild(videoSources[videoSources.length - 1]);
+
+  sourcesElem.removeChild(videoSources[videoSources.length - 1]);
+  remoteElem.removeChild(videoRemotes[videoRemotes.length - 1]);
   videoSources[videoSources.length - 1].srcObject = null;
+  videoRemotes[videoRemotes.length - 1].srcObject = null;
   videoSources.pop();
+  videoRemotes.pop();
 }
 function dumpOptionsInfo() {
   const videoTrack = (videoSources[videoSources.length - 1]
@@ -126,5 +96,3 @@ function dumpOptionsInfo() {
   console.info("Track constraints:");
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
-
-UpdateButtons();
